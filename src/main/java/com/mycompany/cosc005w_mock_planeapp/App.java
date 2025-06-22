88436463c728b43ad55c2d86ddd799f0ba71c167
Natalie451 <81078733+Natalie451@users.noreply.github.com>
2025-06-22 22:35:56 +0100

    Completed task 1. Changed the number of seats.

--- src/main/java/com/mycompany/cosc005w_mock_planeapp/App.java
+++ src/main/java/com/mycompany/cosc005w_mock_planeapp/App.java
@@ -25,10 +25,10 @@ public class App {
 
     public static void initialiseRows() {
         planeSeats = new int[4][]; // total rows - multidimensional array
-        planeSeats[0] = new int[18]; // row 1 - initialised at 0 all available
-        planeSeats[1] = new int[20]; // row 2 - initialised at 0 all available
-        planeSeats[2] = new int[20]; // row 2 - initialised at 0 all available
-        planeSeats[3] = new int[18]; // row 2 - initialised at 0 all available
+        planeSeats[0] = new int[16]; // row 1 - initialised at 0 all available
+        planeSeats[1] = new int[22]; // row 2 - initialised at 0 all available
+        planeSeats[2] = new int[22]; // row 2 - initialised at 0 all available
+        planeSeats[3] = new int[16]; // row 2 - initialised at 0 all available
         pricePerRow = new int[4];
         pricePerRow[0] = 50;
         pricePerRow[1] = 80;
